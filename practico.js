ad4413d633b28ceb7d957cc1faf77b59cb665d1e
Faclle <115706518+Faclle@users.noreply.github.com>
2023-08-07 17:31:42 -0300

    segundaPreEntrega
    SEGUNDA PRE ENTREGA

---
+++ practico.js
@@ -0,0 +1,16 @@
+alert ('POR FAVOR INGRESE LOS DATOS SOLICITADOS, ESTO ES UNA PAGINA DE DISENO')
+
+var nombre = prompt("Nombre?:","");
+document.write("<p>Bienvenido, " + nombre + "</p>");
+
+var nombre = prompt("compra o consulta?:","");
+document.write("<p>desea, " + nombre + "</p>");
+
+var nombre = prompt("empresa o particular?:","");
+document.write("<p>atencion para, " + nombre + "</p>");
+
+var nombre = prompt("decoracion para?:","");
+document.write("<p>decorar, " + nombre + "</p>");
+
+var nombre = prompt("forma de pago?:","");
+document.write("<p>Paga con tarjeta, " + nombre + "</p>");
